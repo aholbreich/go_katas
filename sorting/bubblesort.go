70f65041b47e92f889b1a0ae29df09e691445db2
Alexander Holbreich <alexander@holbreich.org>
2024-11-10 14:42:02 +0100

    docs: Bubblesort.go

--- sorting/bubblesort.go
+++ sorting/bubblesort.go
@@ -1,6 +1,18 @@
 package sorting
 
-// O(n2)
+/*
+Bubble Sort is a simple comparison-based sorting algorithm.
+Steps:
+
+ 1. Start at the beginning. Repeatedly compare each pair of adjacent elements and swaps them if they are in the
+    wrong order.
+
+ 2. Repeats this process for the remaining 'unsorted portion' (n-1) of the array until no swaps are needed.
+
+In effect every iteration "bubbles" the largest unsorted element to its correct position at the end of the array after each full iteration.
+
+Complexity for all cases is of O(n^2). Not efficient, not recommended for usage for large datasets.
+*/
 func BubbleSort(arr []int) {
 	n := len(arr)
 	for i := 0; i < n-1; i++ {
